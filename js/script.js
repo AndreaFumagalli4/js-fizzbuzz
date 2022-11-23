d9dc53e79c578755e85aa36a780901c53307fa33
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-23 14:55:09 +0100

    add index.html, style.css and script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Consegna:
+    Scrivi un programma che stampi in console i numeri da 1 a 100:
+    ma che per i multipli di 3 stampi “Fizz” al posto del numero e
+    per i multipli di 5 stampi “Buzz”.
+    Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+    HOW TO DO:
+    1. Scrivere con FOR stampa numeri da 1 a 100 e mostrarli nella console;
+        1.1 SE un numero è multiplo di 3 ==> mostrare nella console 'Fizz';
+        1.2 SE un numero è multiplo di 5 ==> mostrare nella console 'Buzz';
+        1.3 SE un numero è sia multiplo di 3 che di 5 ==> mostrare nella console 'FizzBuzz';
+ */
